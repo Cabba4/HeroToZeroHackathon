ef46d77422d1bb265d3181d6ab87247294c27495
vqanar <anmol.arora@tuni.fi>
2022-03-12 16:00:03 +0200

    Login page enabled

--- RouteSuggester/app/src/main/java/com/example/myroute/loginPage.java
+++ RouteSuggester/app/src/main/java/com/example/myroute/loginPage.java
@@ -2,7 +2,10 @@ package com.example.myroute;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class loginPage extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class loginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+    }
+
+    public void loginNow(View view) {
+        EditText username = (EditText)findViewById(R.id.editTextTextPersonName);
+        EditText password = (EditText)findViewById(R.id.editTextTextPassword);
+
+    }
+
+    public void openRegister(View view) {
+        Intent intent = new Intent(this,registerPerson.class);
+        startActivity(intent);
     }
 }
---
+++ RouteSuggester/app/src/main/java/com/example/myroute/registerPerson.java
@@ -0,0 +1,14 @@
+package com.example.myroute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registerPerson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_person);
+    }
+}
